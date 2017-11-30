5abadab94e789cb7433acf881ac5207ab9d246cd
Yann CHAYSINH <yann.chaysinh@gmail.com>
2017-11-30 13:34:48 +0000

    1st commit

---
+++ intro.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import json
+#j = json.loads('{"one" : "1", "two" : "2", "three" : "3"}')
+#print j['two']
+
+
+def intro():
+    print ("###################")
+    print ("##   HELLO       ##")
+    print ("###################")
+    return;
+
+# Now you can call printme function
+myjson = json.loads(open('test.json').read())
+intro()
+print ("my name is " + myjson['lastname'] + ", " + myjson['firstname'] + " " + myjson['lastname'])
