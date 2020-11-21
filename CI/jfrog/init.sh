2a57d8c8f3462d4fccf7a2292d3f5ed4faf688aa
cyan21 <yann.chaysinh@gmail.com>
2020-11-21 21:45:01 +0100

    added init pipeline

--- CI/jfrog/init.sh
+++ CI/jfrog/init.sh
@@ -3,11 +3,27 @@
 
 # requires : XRAY POLICY (see policy name in watch.json)
 
+usage() {
+    echo "$0 -u <JPD_URL> -l <ADMIN_USER> -p <ADMIN_PASS>"
+}
+
 JPD_URL="http://artifactory-eu-yannc3-0.soleng-emea-staging.jfrog.team"
 ADMIN_USER="admin"
-ADMIN_PASS="password"
 BUILD_NAMES="app_python"  #list with comma as a separator
 
+while getopts "u:l:p:" option; do
+    case "${option}" in
+        u) JPD_URL=${OPTARG} ;;
+        l) ADMIN_USER=${OPTARG} ;;
+        p) ADMIN_PASS=${OPTARG} ;;
+        *) usage && exit 1;;
+    esac
+done
+
+if [ -z "$ADMIN_PASS" ]; then
+    echo "[ERROR] missing ADMIN PASSWORD"
+    exit 1
+fi
 
 creds="-u$ADMIN_USER:$ADMIN_PASS"
 
